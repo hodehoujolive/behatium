cfe026b29afe4b4a5a5683d3bb9dc8c8cd365fef
Hodehou Jolivé <jolive.hodehou@openware.online>
2020-06-25 12:19:28 +0000

    Update features/bootstrap/WebContext.php

--- features/bootstrap/WebContext.php
+++ features/bootstrap/WebContext.php
@@ -26,51 +26,54 @@ class WebContext extends MinkContext implements SnippetAcceptingContext {
     public function iWaitForSeconds($secondes){
         $this->getSession()->wait($secondes * 1000);
     }
-    
+
+
+
+
     /**
    * @BeforeSuite
    * save in time the date for the begin of tests
    */
-    public static function setup(BeforeSuiteScope $scope) {
-        $GLOBALS['time'] = new DateTime(date("Y-m-d H:i:s"));
-    }
+  public static function setup(BeforeSuiteScope $scope) {
+    $GLOBALS['time'] = new DateTime(date("Y-m-d H:i:s"));
+}
 
-    /**
-   * @AfterSuite
-   * find the duration of all the test
-   */
-    public static function teardown(AfterSuiteScope $scope) {
-        $endTime = new DateTime(date("Y-m-d H:i:s"));
-        $var = $GLOBALS['time']->diff($endTime);
-        print_r("Duration of the test: ".$var->format("%H:%I:%S"));
-    }
+/**
+* @AfterSuite
+* find the duration of all the test
+*/
+public static function teardown(AfterSuiteScope $scope) {
+    $endTime = new DateTime(date("Y-m-d H:i:s"));
+    $var = $GLOBALS['time']->diff($endTime);
+    print_r("Duration of the test: ".$var->format("%H:%I:%S"));
+}
 
-    /**
-   * @AfterStep
-   */
-    public function takeScreenShotAfterFailedStep(afterStepScope $scope) {
-        $path_failed = $this->parameters['path_failed']."/".date('Y')."/".date('m')."/".date('d')."/";
-        $path_successed = $this->parameters['path_successed']."/".date('Y')."/".date('m')."/".date('d')."/";
-        if (99 === $scope->getTestResult()->getResultCode()) {
-            $driver = $this->getSession()->getDriver();
-            if (!($driver instanceof Selenium2Driver)) {
-                return;
-            }
-            if (!is_dir($path_failed)) {
-                // dir doesn't exist, make it
-                mkdir($path_failed, 0755, true);
-            }
-            $currentTime = time();
-            file_put_contents($path_failed.date('H',$currentTime).'h'.date('i',$currentTime).'m'.date('s',$currentTime).'s'.'.png',$this->getSession()->getDriver()->getScreenshot());
-        } else {
-            if (!is_dir($path_successed)) {
-                // dir doesn't exist, make it (and third parameter will create any missing directory structure )
-                mkdir($path_successed, 0755, true);
-            }
-            $currentTime = time();
-            file_put_contents($path_successed.date('H',$currentTime).'h'.date('i',$currentTime).'m'.date('s',$currentTime).'s'.'.png',$this->getSession()->getDriver()->getScreenshot());
+/**
+* @AfterStep
+*/
+public function takeScreenShotAfterFailedStep(afterStepScope $scope) {
+    $path_failed = $this->parameters['path_failed']."/".date('Y')."/".date('m')."/".date('d')."/";
+    $path_successed = $this->parameters['path_successed']."/".date('Y')."/".date('m')."/".date('d')."/";
+    if (99 === $scope->getTestResult()->getResultCode()) {
+        $driver = $this->getSession()->getDriver();
+        if (!($driver instanceof Selenium2Driver)) {
+            return;
         }
+        if (!is_dir($path_failed)) {
+            // dir doesn't exist, make it
+            mkdir($path_failed, 0755, true);
+        }
+        $currentTime = time();
+        file_put_contents($path_failed.date('H',$currentTime).'h'.date('i',$currentTime).'m'.date('s',$currentTime).'s'.'.png',$this->getSession()->getDriver()->getScreenshot());
+    } else {
+        if (!is_dir($path_successed)) {
+            // dir doesn't exist, make it (and third parameter will create any missing directory structure )
+            mkdir($path_successed, 0755, true);
+        }
+        $currentTime = time();
+        file_put_contents($path_successed.date('H',$currentTime).'h'.date('i',$currentTime).'m'.date('s',$currentTime).'s'.'.png',$this->getSession()->getDriver()->getScreenshot());
     }
+}
 
 
             /**
@@ -716,25 +719,142 @@ class WebContext extends MinkContext implements SnippetAcceptingContext {
         $element->setValue($value);
     }
 
-            /**
-     *
-     * @When I mouse over the element with xpath :xpath
-     * Click on the element with the provided xpath query
-     */
-    public function iMouseOverTheElementWithXPath($xpath)
-    {
-        $time = 3500;
-        $this->getSession()->wait($time);
-
-        $session = $this->getSession();
-        $element = $session->getPage()->find(
-            'xpath',
-            $session->getSelectorsHandler()->selectorToXpath('xpath', $xpath)
-        );
-        if (null === $element) {
-            throw new \InvalidArgumentException(sprintf('Could not evaluate XPath: "%s"', $xpath));
-        }
-        $element->MouseOver();
+    	/**
+	 * @Given /^I scroll to the bottom$/
+	 */
+	public function iScrollToBottom() {
+		$javascript = 'window.scrollTo(0, Math.max(document.documentElement.scrollHeight, document.body.scrollHeight, document.documentElement.clientHeight));';
+		$this->getSession()->executeScript($javascript);
     }
+    
+    	/**
+	 * @Given /^I scroll to the top$/
+	 */
+	public function iScrollToTop() {
+		$this->getSession()->executeScript('window.scrollTo(0,0);');
+    }
+    
+      /**
+   * Y would be the way to up and down the page. A good default for X is 0
+   *
+   * @Given /^I scroll to x "([^"]*)" y "([^"]*)" coordinates of page$/
+   */
+  public function iScrollToXYCoordinatesOfPage($arg1, $arg2) {
+    $function = "(function(){
+              window.scrollTo($arg1, $arg2);
+            })()";
+    try {
+      $this->getSession()->executeScript($function);
+    }
+    catch(Exception $e) {
+      throw new \Exception("ScrollIntoView failed");
+    }
+  }
+
+  /**
+   * Performs a mouseover event on a link with specified text.
+   *
+   * @When I hover over the link :arg1
+   * @param string $linkText
+   */
+  public function iHoverOverTheLink($linkText){
+    $element = $this->getSession()->getPage()->findLink($linkText);
+    if ($element == NULL){
+      throw new \InvalidArgumentException(sprintf('Cannot find text: "%s"', $linkText));
+    }
+    $element->mouseOver();
+  }
+
+  
+ /**
+   *
+   * @When I click the link :arg1
+   * @param string $linkText
+   */
+  public function iClickTheLink($linkText){
+    $element = $this->getSession()->getPage()->findLink($linkText);
+    if ($element == NULL){
+      throw new \InvalidArgumentException(sprintf('Cannot find text: "%s"', $linkText));
+    }
+    $element->click();
+  }
+  
+
+   /**
+   * Check if the port is 443(https) or 80(http) / secure or not.
+   *
+   * @Then /^the page is secure$/
+   */
+  public function thePageIsSecure()
+  {
+    $current_url = $this->getSession()->getCurrentUrl();
+    if(strpos($current_url, 'https') === false) {
+      throw new Exception('Page is not using SSL and is not Secure');
+    }
+  } 
+
+
+    /**
+   * Setting screen size to 320x900 (mobile portrait)
+   *
+   * @Given /^the size is mobile portrait/
+   */
+  public function theSizeIsMobilePortrait()
+  {
+    $this->getSession()->resizeWindow(320, 900, 'current');
+  }
+
+
+    /**
+   * Setting screen size to 640x900 (mobile landscape)
+   *
+   * @Given /^the size is mobile landscape/
+   */
+  public function theSizeIsMobileLandscape()
+  {
+    $this->getSession()->resizeWindow(640, 900, 'current');
+  }
+
+    /**
+   * Setting custom size of the screen using width and height parameters
+   *
+   * @Given /^the custom size is "([^"]*)" by "([^"]*)"$/
+   */
+  public function theCustomSizeIs($width, $height)
+  {
+    $this->getSession()->resizeWindow($width, $height, 'current');
+  }
+
+  /**
+   * Setting screen size to 1400x900 (desktop)
+   *
+   * @Given /^the size is desktop/
+   */
+  public function theSizeIsDesktop()
+  {
+    $this->getSession()->resizeWindow(1400, 900, 'current');
+  }
+
+  /**
+   * Setting screen size to 1024x900 (tablet landscape)
+   *
+   * @Given /^the size is tablet landscape/
+   */
+  public function theSizeIsTabletLandscape()
+  {
+    $this->getSession()->resizeWindow(1024, 900, 'current');
+  }
+
+  /**
+   * Setting screen size to 768x900 (tablet portrait)
+   *
+   * @Given /^the size is tablet portrait/
+   */
+  public function theSizeIsTabletPortrait()
+  {
+    $this->getSession()->resizeWindow(768, 900, 'current');
+  }
+  
+
 
 }
